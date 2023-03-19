cd768775a10ca9b861ccbcdf3a877216c195d608
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-19 10:24:28 +0300

    Created the root endpoint for the project

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+@app.get("/")
+async def root() -> dict:
+    """The root api endpoint
+
+    Returns:
+        dict: A message for when the code is successfully executed
+    """
+    return {"Message": "Hello World"}
