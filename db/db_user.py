114f5e0b9056ba7ed7f310e18a14b4487022ce9e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 04:23:47 +0300

    Not done writing the logic for the file

--- db/db_user.py
+++ db/db_user.py
@@ -1,1 +1,12 @@
 """The file that contains teh ORM functionality"""
+from sqlalchemy.orm.session import Session
+from schemas import UserBase
+from db.models import DbUser
+
+
+def create_user(_db: Session, request: UserBase):
+    new_user = DbUser(
+        username=request.username,
+        email=request.email,
+        password=request.password,
+    )
