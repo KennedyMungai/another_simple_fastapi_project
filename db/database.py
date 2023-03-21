fd810540bf90f22e2cad5a3d17292816fa195871
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 03:32:32 +0300

    Added a url to the database file

--- db/database.py
+++ db/database.py
@@ -2,3 +2,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+
+SQLITE_DATABASE_URL = "sqlite:///./blog.db"
