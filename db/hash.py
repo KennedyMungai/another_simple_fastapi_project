82886631044c07eb0e469d3bd3047fa1aa3b2bba
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 04:34:26 +0300

    Added self arguments to the class functions

--- db/hash.py
+++ db/hash.py
@@ -6,8 +6,8 @@ password_cxt = CryptContext(schemes='bcrypt', deprecated='auto')
 
 
 class Hash():
-    def bcrypt(password: str):
+    def bcrypt(self, password: str):
         return password_cxt.hash(password)
 
-    def verify(hashed_password, plain_password):
+    def verify(self, hashed_password, plain_password):
         return password_cxt.verify(plain_password, hashed_password)
