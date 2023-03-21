f8116aca3eee860f8ef28e0d726c7c90afc65e3c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 05:13:05 +0300

    Added a docstring to the create_user endpoint

--- routers/user.py
+++ routers/user.py
@@ -13,4 +13,13 @@ router = APIRouter(prefix="/user", tags=['user'])
 # Create User
 @router.post("/")
 async def create_user(_request: UserBase, _db: Session = Depends(get_db)):
+    """The create user endpoint
+
+    Args:
+        _request (UserBase): The user data
+        _db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        _type_: _description_
+    """
     return db_user.create_user(_db, _request)
