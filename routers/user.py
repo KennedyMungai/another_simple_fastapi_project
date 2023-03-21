24538d2e32c5978a38b3e128cd534e48a0fcc69d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 05:09:21 +0300

    Created the skeleton of the create user endpoint

--- routers/user.py
+++ routers/user.py
@@ -1,5 +1,14 @@
 """The file to contain the logic for the user API endpoint"""
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm.session import Session
+from db.database import get_db
+from schemas import UserBase
 
 
 router = APIRouter(prefix="/user", tags=['user'])
+
+
+# Create User
+@router.post("/")
+async def create_user(_request: UserBase, _db: Session = Depends(get_db)):
+    pass
