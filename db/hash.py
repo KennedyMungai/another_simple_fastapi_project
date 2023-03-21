a6e86be4a293d1ad2dc66b41f0974daf05502a80
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 04:31:15 +0300

    Wrote a function to encrypt the password

--- db/hash.py
+++ db/hash.py
@@ -3,3 +3,8 @@ from passlib.context import CryptContext
 
 
 password_cxt = CryptContext(schemes='bcrypt', deprecated='auto')
+
+
+class Hash():
+    def bcrypt(password: str):
+        return password_cxt.hash(password)
