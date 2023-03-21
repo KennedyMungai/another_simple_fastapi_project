3534d8d2757d4dcd6f7441629725407ef17f2291
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 03:54:17 +0300

    Added a docstring to the DbUser class

--- db/models.py
+++ db/models.py
@@ -4,6 +4,11 @@ from db.database import Base
 
 
 class DbUser(Base):
+    """The template for the User data
+
+    Args:
+        Base (Function): Parent class
+    """
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, nullable=False)
