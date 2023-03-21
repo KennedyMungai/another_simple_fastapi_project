8a8b95e3218b864f34f028301df020d74e065807
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 03:53:13 +0300

    Created te dbUser model

--- db/models.py
+++ db/models.py
@@ -1,1 +1,11 @@
 """A python file to define the models of the data to be put inside the database"""
+from sqlalchemy import Column, Integer, String, DateTime
+from db.database import Base
+
+
+class DbUser(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
