16f849d40104c61fae6bd529fbc2023874a0cabb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 04:44:54 +0300

    Passed on the password string to be hashed

--- db/db_user.py
+++ db/db_user.py
@@ -2,11 +2,12 @@
 from sqlalchemy.orm.session import Session
 from schemas import UserBase
 from db.models import DbUser
+from db.hash import Hash
 
 
 def create_user(_db: Session, request: UserBase):
     new_user = DbUser(
         username=request.username,
         email=request.email,
-        password=request.password,
+        password=Hash(request.password)
     )
