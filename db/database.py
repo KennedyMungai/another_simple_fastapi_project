059ce017ba6bccdeafe1916dbb60fc0cec5fc98c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 03:42:52 +0300

    Created a function that gets us a database session

--- db/database.py
+++ db/database.py
@@ -13,3 +13,11 @@ engine = create_engine(SQLITE_DATABASE_URL, connect_args={
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=Engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
