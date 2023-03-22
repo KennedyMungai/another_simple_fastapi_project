d191b371c68fdd81cbdec584777b75e0198abd04
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 03:07:10 +0300

    Created an endpoint to retrieve all the users from a database

--- routers/user.py
+++ routers/user.py
@@ -1,11 +1,12 @@
 """The file to contain the logic for the user API endpoint"""
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm.session import Session
 
 from db import db_user
 from db.database import get_db
 from schemas import UserBase, UserDisplay
-
 
 router = APIRouter(prefix="/user", tags=['user'])
 
@@ -23,3 +24,16 @@ async def create_user(_request: UserBase, _db: Session = Depends(get_db)):
         _type_: _description_
     """
     return db_user.create_user(_db, _request)
+
+
+@router.get("/", response_model=List[UserDisplay])
+async def retrieve_all_users(_db: Session = Depends(get_db)):
+    """The endpoint to retrieve all the users from the database
+
+    Args:
+        _db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        List: A list of users in the UserDisplay template
+    """
+    return db_user.retrieve_all_users(_db)
