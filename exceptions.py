2925340db8c6f977798743ddc7433bf7b383fc1e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 02:53:57 +0300

    Added a docstring

--- exceptions.py
+++ exceptions.py
@@ -2,5 +2,11 @@
 
 
 class StoryException(Exception):
+    """A class that is the themplate for Story Exceptiond
+
+    Args:
+        Exception (Class): The main exception class
+    """
+
     def __init__(self, name: str) -> None:
         self.name = name
