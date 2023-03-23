6102fa65f63a442d30f0d908745dff6f6cc113f8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 03:21:18 +0300

    Reorganized the articles

--- routers/article.py
+++ routers/article.py
@@ -1,8 +1,8 @@
 """The router file for the article"""
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm.session import Session
-from schemas import ArticleBase, ArticleDisplay
-from db.database import get_db
 
+from db.database import get_db
+from schemas import ArticleBase, ArticleDisplay
 
 router = APIRouter(prefix="/article", tags=['article'])
