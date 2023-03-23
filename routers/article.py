30bc860bf4deabe4928964495d101c2b71d450dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 03:24:57 +0300

    Defined the createArticle andpoint

--- routers/article.py
+++ routers/article.py
@@ -7,3 +7,8 @@ from db.database import get_db
 from schemas import ArticleBase, ArticleDisplay
 
 router = APIRouter(prefix="/article", tags=['article'])
+
+
+@router.post("/", response_model=ArticleDisplay)
+def create_article(_request: ArticleBase, _db: Session = Depends(get_db)):
+    return db_article.create_article(_db, _request)
