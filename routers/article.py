68d3c4e0ebe99c4eb75dcaf74c6e9dc2ad839028
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 03:21:01 +0300

    Imported ArticleBase and ArticleDisplay from schemas

--- routers/article.py
+++ routers/article.py
@@ -1,7 +1,7 @@
 """The router file for the article"""
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm.session import Session
-
+from schemas import ArticleBase, ArticleDisplay
 from db.database import get_db
 
 
