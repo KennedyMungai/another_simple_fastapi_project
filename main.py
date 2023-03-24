30568cbb159123cba531cc0d22bf1b8fa4a67d3f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 04:39:19 +0300

    Got rid of the product router and any reference to it in the main file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from db import models
 from db.database import engine
-from routers import article, blog_get, blog_post, product, user
+from routers import article, blog_get, blog_post, user
 
 app = FastAPI()
 
@@ -23,7 +23,6 @@ app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
-app.include_router(product.router)
 
 models.Base.metadata.create_all(engine)
 
