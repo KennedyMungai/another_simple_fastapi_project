63b5755912f20082c6402864a4d6c425cd939e9d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 05:18:15 +0300

    Added some basic setup code for the get_token function

--- auth/authentication.py
+++ auth/authentication.py
@@ -1,5 +1,16 @@
 """Contains an authentication endpoint"""
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm.session import Session
+
+from db.database import get_db
 
 
 router = APIRouter(tags=['authentication'])
+
+
+@router.post('/token')
+def get_token(
+    _request: OAuth2PasswordRequestForm = Depends()
+    _db: Session = Depends(get_db)
+):
