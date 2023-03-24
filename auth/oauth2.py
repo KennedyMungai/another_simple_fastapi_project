3a10c599036fbc0f4052ccd72e493409a1f2f987
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 05:03:22 +0300

    Wrote the access token code

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -1,5 +1,30 @@
 """A file containing auth code"""
 from fastapi.security import OAuth2PasswordBearer
+from typing import Optional
+from datetime import datetime, timedelta
+from jose import jwt
+import os
+from dotenv import load_dotenv, find_dotenv
+
+_env = find_dotenv(load_dotenv())
 
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='token')
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
